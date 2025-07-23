ffca5f221bef277964bdc4051594d8b749b2059f
Colin Farkas <colin.farkas@hotmail.se>
2025-07-23 19:24:16 +0200

    Adds font-face Bricolage Grotesque

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import DesktopOverview from '$lib/components/Pages/Overview/DesktopOverview.svelte';
 	import MobileOverview from '$lib/components/Pages/Overview/MobileOverview.svelte';
 
-
 	import type { WeatherOverview } from '$lib/types/weather.js';
 	import { screenSize } from '$lib/globals/screenState';
 	import { onDestroy } from 'svelte';
@@ -12,20 +11,19 @@
 		data: WeatherOverview;
 	}
 	let { data }: PageProps = $props();
-	
+
 	// Logic
 	let _isDesktop = $state(false);
 
-	let unsubscribe = screenSize.subscribe((value) => _isDesktop = value.isDesktop);
+	let unsubscribe = screenSize.subscribe((value) => (_isDesktop = value.isDesktop));
 
-	onDestroy(unsubscribe)
+	onDestroy(unsubscribe);
 </script>
-
 
 <article class="overview">
 	{#if _isDesktop}
 		<DesktopOverview {data} />
-		{:else}
+	{:else}
 		<MobileOverview {data} />
 	{/if}
 </article>
