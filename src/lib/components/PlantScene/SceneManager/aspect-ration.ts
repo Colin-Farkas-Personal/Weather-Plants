034c35798c35118c576d85ab756ee17f757259e8
Colin Farkas <colin.farkas@hotmail.se>
2025-09-15 21:54:56 +0200

    Fixes aspect ratio being infinity

--- src/lib/components/PlantScene/SceneManager/aspect-ration.ts
+++ src/lib/components/PlantScene/SceneManager/aspect-ration.ts
@@ -2,17 +2,17 @@ type Orientation = 'landscape' | 'portrait';
 type AspectRatio = number;
 
 interface GetScreenOrientationReturn {
-    aspectRatio: AspectRatio;
-    orientation: Orientation;
+	aspectRatio: AspectRatio;
+	orientation: Orientation;
 }
 
 function getScreenOrientation(): GetScreenOrientationReturn {
-    const aspectRatio = document.body.clientWidth / document.body.clientHeight;
-    
-    return {
-        aspectRatio,
-        orientation: aspectRatio > 1 ? 'landscape' : 'portrait'
-    };
+	const aspectRatio = window.innerWidth / window.innerHeight;
+
+	return {
+		aspectRatio,
+		orientation: aspectRatio > 1 ? 'landscape' : 'portrait',
+	};
 }
 
 export { getScreenOrientation };
