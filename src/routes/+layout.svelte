fd5f08cb633686124f6db146b3a5ac00556c96e9
Colin Farkas <colin.farkas@hotmail.se>
2025-10-31 12:49:44 +0100

    Adds heading meta tags in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,12 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+	<meta name="apple-mobile-web-app-title" content="Weather Plants" />
+	<link rel="apple-touch-icon" href="/icons/icon-large.png" />
+	<meta name="theme-color" content="#ffffff" />
+</svelte:head>
+
 {@render children?.()}
