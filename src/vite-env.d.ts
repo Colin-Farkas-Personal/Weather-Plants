5f0c822ebe43c5b5b4708435eee29deae183b8ff
Colin Farkas <colin.farkas@hotmail.se>
2025-10-31 14:23:50 +0100

    Working PWA: Fixes vite config and adds webManifestLink in layout

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 /// <reference types="unplugin-icons/types/svelte" />
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/info" />
