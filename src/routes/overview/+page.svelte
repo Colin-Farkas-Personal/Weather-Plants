6913f85852616db64a9ba44ef68e453ac73f4abd
Colin <colin.farkas@hotmail.se>
2026-03-10 21:54:51 +0100

    Fixes incorrect name

--- src/routes/overview/+page.svelte
+++ src/routes/overview/+page.svelte
@@ -120,7 +120,7 @@
 			currentHour: hourValue,
 			sunriseHour: astro.sunriseHour,
 			sunsetHour: astro.sunsetHour,
-			isForecastDisplayActiveActive: $forecastDisplay,
+			isForecastDisplayActive: $forecastDisplay,
 		});
 	}
 
